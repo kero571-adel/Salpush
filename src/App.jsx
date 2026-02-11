0741a0fac1da8af4bba3698a0cbcfa5204fb9445
keroles adel <kerolesadel297@gmail.com>
2026-02-11 18:18:19 +0200

    add navBar ,edit app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
-import { AppBar, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import "./App.css";
-
+import Nav from "./navBar";
 function App() {
   return (
     <Container>
-      <AppBar
-        sx={{
-          background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-          display: "flex",
-          justifyContent: "space-between",
-        }}
-      >
-        <Box>
-          <img src="/" alt="logo" />
-        </Box>
-      </AppBar>
+      <Nav/>
     </Container>
   );
 }
---
+++ src/navBar.jsx
@@ -0,0 +1,9 @@
+import { Stack } from "@mui/material";
+
+export default function Nav() {
+    return (
+        <Stack>
+            
+        </Stack>
+    );
+}
