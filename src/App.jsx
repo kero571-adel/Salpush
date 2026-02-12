f924ec2b51ed458a13155f60419954895beb04e5
keroles adel <kerolesadel297@gmail.com>
2026-02-13 01:23:46 +0200

    add nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { Container } from "@mui/material";
+import NavBarRight from "./navBarRight";
 import "./App.css";
 import Nav from "./navBar";
+import Search from "./search";
+import NavBarLeft from "./navBarLeft";
 function App() {
   return (
-    <Container>
-      <Nav/>
-    </Container>
+    <>
+      <Nav />
+      {/* <NavBarRight/> */}
+      {/* <Search/> */}
+      {/* <NavBarLeft open={true}/> */}
+    </>
   );
 }
 export default App;
